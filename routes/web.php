635f98fb3a6fe75ba586a48d441d447d718df9bb
kazuki3 <okymkzk0816@gmail.com>
2019-06-06 16:35:34 +0900

    Create database

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('index');
 });
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
